1e0412f639f4ef83e99ac2b092b3d45c3f129fc5
a.pryanikov <a.pryanikov@prosoftsystems.ru>
2018-10-24 18:16:43 +0500

    Реализация тест кейса

---
+++ SandBox777/SandBox777/LogAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SandBox777
+{
+    public class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.EndsWith(".SLF", StringComparison.CurrentCultureIgnoreCase))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ SandBox777/SandBox777/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SandBox777
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
